81238665d498773421605644e065259a7f8f178a
dankvn <cjmayk@hotmail.com>
2023-06-13 15:46:08 -0500

    test-1

--- pages/failed.js
+++ pages/failed.js
@@ -7,11 +7,11 @@ const Failed = () => {
         <div className="min-h-[650px] flex items-center">
             <Wrapper>
                 <div className="max-w-[600px] rounded-lg p-5 border border-black mx-auto flex flex-col">
-                    <div className="text-2xl font-bold">Payment failed!</div>
+                    <div className="text-2xl font-bold">¡Pago Fallido!</div>
                     <div className="text-base mt-5">
-                        For any product related query, drop an email to
+                    Para cualquier consulta relacionada con el producto, envíe un correo electrónico a
                     </div>
-                    <div className="underline">shoeshopcontact@shop.com</div>
+                    <div className="underline">sopertest@gmail.com</div>
 
                     <Link href="/" className="font-bold mt-5">
                         Continue Shopping
--- pages/success.js
+++ pages/success.js
@@ -8,15 +8,16 @@ const Success = () => {
             <Wrapper>
                 <div className="max-w-[600px] rounded-lg p-5 border border-black mx-auto flex flex-col">
                     <div className="text-2xl font-bold">
-                        Thanks for shopping with us!
+                        Gracias por comprar con nosotros!
+
                     </div>
                     <div className="text-lg font-bold mt-2">
-                        Your order has been placed successfully.
+                        Su pedido ha sido realizado con éxito.
                     </div>
                     <div className="text-base mt-5">
-                        For any product related query, drop an email to
+                        Para cualquier consulta relacionada con el producto, envíe un correo electrónico a
                     </div>
-                    <div className="underline">shoeshopcontact@shop.com</div>
+                    <div className="underline">sopertest@gmail.com</div>
 
                     <Link href="/" className="font-bold mt-5">
                         Continue Shopping
