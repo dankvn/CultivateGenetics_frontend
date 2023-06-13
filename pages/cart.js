57921b9579451d9c07f9173ec2d8ec69d8e2a23a
dankvn <cjmayk@hotmail.com>
2023-06-13 12:02:25 -0500

    test-1

--- pages/cart.js
+++ pages/cart.js
@@ -4,129 +4,127 @@ import Link from "next/link";
 import Wrapper from "@/components/Wrapper";
 import CartItem from "@/components/CartItem";
 import { useSelector } from "react-redux";
-
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 import { makePaymentRequest } from "@/utils/api";
 import { loadStripe } from "@stripe/stripe-js";
 const stripePromise = loadStripe(
-    process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
 );
 
 const Cart = () => {
-    const [loading, setLoading] = useState(false);
-    const { cartItems } = useSelector((state) => state.cart);
+  const [loading, setLoading] = useState(false);
+  const { cartItems } = useSelector((state) => state.cart);
 
-    const subTotal = useMemo(() => {
-        return cartItems.reduce(
-            (total, val) => total + val.attributes.price,
-            0
-        );
-    }, [cartItems]);
+  const subTotal = useMemo(() => {
+    return cartItems.reduce((total, val) => total + val.attributes.price, 0);
+  }, [cartItems]);
 
-    const handlePayment = async () => {
-        try {
-            setLoading(true);
-            const stripe = await stripePromise;
-            const res = await makePaymentRequest("/api/orders", {
-                products: cartItems,
-            });
-            await stripe.redirectToCheckout({
-                sessionId: res.stripeSession.id,
-            });
-        } catch (error) {
-            setLoading(false);
-            console.log(error);
-        }
-    };
+  const handlePayment = async () => {
+    try {
+      setLoading(true);
+      const stripe = await stripePromise;
+      const res = await makePaymentRequest("/api/orders", {
+        products: cartItems,
+      });
+      await stripe.redirectToCheckout({
+        sessionId: res.stripeSession.id,
+      });
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
 
-    return (
-        <div className="w-full md:py-20">
-            <Wrapper>
-                {cartItems.length > 0 && (
-                    <>
-                        {/* HEADING AND PARAGRAPH START */}
-                        <div className="text-center max-w-[800px] mx-auto mt-8 md:mt-0">
-                            <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
-                            Carrito De Compra
-                            </div>
-                        </div>
-                        {/* HEADING AND PARAGRAPH END */}
+  return (
+    <div className="w-full md:py-20">
+      <Wrapper>
+        {cartItems.length > 0 && (
+          <>
+            {/* HEADING AND PARAGRAPH START */}
+            <div className="text-center max-w-[800px] mx-auto mt-8 md:mt-0">
+              <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
+                Carrito De Compra
+              </div>
+            </div>
+            {/* HEADING AND PARAGRAPH END */}
 
-                        {/* CART CONTENT START */}
-                        <div className="flex flex-col lg:flex-row gap-12 py-10">
-                            {/* CART ITEMS START */}
-                            <div className="flex-[2]">
-                                <div className="text-lg font-bold">
-                                    Artículos del carrito
-                                </div>
-                                {cartItems.map((item) => (
-                                    <CartItem key={item.id} data={item} />
-                                ))}
-                            </div>
-                            {/* CART ITEMS END */}
+            {/* CART CONTENT START */}
+            <div className="flex flex-col lg:flex-row gap-12 py-10">
+              {/* CART ITEMS START */}
+              <div className="flex-[2]">
+                <div className="text-lg font-bold">Artículos del carrito</div>
+                {cartItems.map((item) => (
+                  <CartItem key={item.id} data={item} />
+                ))}
+              </div>
+              {/* CART ITEMS END */}
 
-                            {/* SUMMARY START */}
-                            <div className="flex-[1]">
-                                <div className="text-lg font-bold">Resumen</div>
+              {/* SUMMARY START */}
+              <div className="flex-[1]">
+                <div className="text-lg font-bold">Resumen</div>
 
-                                <div className="p-5 my-5 bg-black/[0.05] rounded-xl">
-                                    <div className="flex justify-between">
-                                        <div className="uppercase text-md md:text-lg font-medium text-black">
-                                            Subtotal
-                                        </div>
-                                        <div className="text-md md:text-lg font-medium text-black">
-                                            ${subTotal}
-                                        </div>
-                                    </div>
-                                    <div className="text-sm md:text-md py-5 border-t border-t-black mt-5">
-                                    El subtotal refleja el precio total de su pedido, incluidos aranceles e impuestos, antes de cualquier descuento aplicable.
-                                     No incluye gastos de envío ni tasas de transacción internacional..
-                                    </div>
-                                </div>
-
-                                {/* BUTTON START */}
-                                <button
-                                    className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75 flex items-center gap-2 justify-center"
-                                    onClick={handlePayment}
-                                >
-                                    Checkout
-                                    {loading && <img src="/spinner.svg" />}
-                                </button>
-                                {/* BUTTON END */}
-                            </div>
-                            {/* SUMMARY END */}
-                        </div>
-                        {/* CART CONTENT END */}
-                    </>
-                )}
-
-                {/* This is empty screen */}
-                {cartItems.length < 1 && (
-                    <div className="flex-[2] flex flex-col items-center pb-[150px] md:-mt-14">
-                        <Image
-                            src="/error.png"
-                            width={300}
-                            height={300}
-                            className="w-[300px] md:w-[400px]"
-                        />
-                        <span className="text-xl font-bold">
-                            Tu carrito esta vacío
-                        </span>
-                        <span className="text-center mt-4">
-                            Parece que no has añadido nada a tu carrito.
-                            <br />
-                            Continúe y explore las categorías principales.
-                        </span>
-                        <Link
-                            href="/"
-                            className="py-4 px-8 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75 mt-8"
-                        >
-                            Seguir Comprando
-                        </Link>
+                <div className="p-5 my-5 bg-black/[0.05] rounded-xl">
+                  <div className="flex justify-between">
+                    <div className="uppercase text-md md:text-lg font-medium text-black">
+                      Subtotal
                     </div>
-                )}
-            </Wrapper>
-        </div>
-    );
+                    <div className="text-md md:text-lg font-medium text-black">
+                      ${subTotal}
+                    </div>
+                  </div>
+                  <div className="text-sm md:text-md py-5 border-t border-t-black mt-5">
+                    El subtotal refleja el precio total de su pedido, incluidos
+                    aranceles e impuestos, antes de cualquier descuento
+                    aplicable. No incluye gastos de envío ni tasas de
+                    transacción internacional..
+                  </div>
+                </div>
+
+                {/* BUTTON START */}
+                <button
+                  className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75 flex items-center gap-2 justify-center"
+                  onClick={handlePayment}
+                >
+                  Checkout
+                  {loading && <img src="/spinner.svg" />}
+                </button>
+                <PayPalScriptProvider options={{ "client-id": "sb" }}>
+                  <PayPalButtons />
+                </PayPalScriptProvider>
+                {/* BUTTON END */}
+              </div>
+              {/* SUMMARY END */}
+            </div>
+            {/* CART CONTENT END */}
+          </>
+        )}
+
+        {/* This is empty screen */}
+        {cartItems.length < 1 && (
+          <div className="flex-[2] flex flex-col items-center pb-[150px] md:-mt-14">
+            <Image
+              src="/error.png"
+              width={300}
+              height={300}
+              className="w-[300px] md:w-[400px]"
+            />
+            <span className="text-xl font-bold">Tu carrito esta vacío</span>
+            <span className="text-center mt-4">
+              Parece que no has añadido nada a tu carrito.
+              <br />
+              Continúe y explore las categorías principales.
+            </span>
+            <Link
+              href="/"
+              className="py-4 px-8 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75 mt-8"
+            >
+              Seguir Comprando
+            </Link>
+          </div>
+        )}
+      </Wrapper>
+    </div>
+  );
 };
 
 export default Cart;
