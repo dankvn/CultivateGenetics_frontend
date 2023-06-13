3ab94fb14709085ba1cddea1897d747b1011956f
dankvn <cjmayk@hotmail.com>
2023-06-13 17:52:24 -0500

    test-1

--- components/MenuMobile.jsx
+++ components/MenuMobile.jsx
@@ -23,13 +23,13 @@ const MenuMobile = ({
     categories,
 }) => {
     return (
-        <ul className="flex flex-col md:hidden font-bold absolute top-[50px] left-0 w-full h-[calc(100vh-50px)] bg-gradient border-t-black text-black">
+        <ul className="flex flex-col md:hidden font-bold absolute top-[50px] left-0 w-full h-[calc(100vh-50px)] bg-gradient  text-black">
             {data.map((item) => {
                 return (
                     <React.Fragment key={item.id}>
                         {!!item?.subMenu ? (
                             <li
-                                className="cursor-pointer py-4 px-5 border-b flex flex-col relative"
+                                className="cursor-pointer py-4 px-5 border-t-black flex flex-col relative"
                                 onClick={() => setShowCatMenu(!showCatMenu)}
                             >
                                 <div className="flex justify-between items-center">
@@ -54,7 +54,7 @@ const MenuMobile = ({
                                                             );
                                                         }}
                                                     >
-                                                        <li className="py-4 px-8 border-t flex justify-between">
+                                                        <li className="py-4 px-8 border-t-black flex justify-between">
                                                             {c.name}
                                                             <span className="opacity-50 text-sm">
                                                                 {`(${c.products.data.length})`}
@@ -68,7 +68,7 @@ const MenuMobile = ({
                                 )}
                             </li>
                         ) : (
-                            <li className="py-4 px-5 border-b">
+                            <li className="py-4 border-t-black ">
                                 <Link
                                     href={item?.url}
                                     onClick={() => setMobileMenu(false)}
